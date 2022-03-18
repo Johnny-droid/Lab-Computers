d5d17991a2798e6e76f3734de1bd424880a6b0c4
Johnny-droid <joaoricardoralves@gmail.com>
2022-03-18 18:02:54 +0000

    timer_get_conf, timer_display_conf, utils_sys_inb

--- lab2/timer.c
+++ lab2/timer.c
@@ -5,6 +5,8 @@
 
 #include "i8254.h"
 
+
+
 int (timer_set_frequency)(uint8_t timer, uint32_t freq) {
   /* To be implemented by the students */
   printf("%s is not yet implemented!\n", __func__);
@@ -33,15 +35,54 @@ void (timer_int_handler)() {
 
 int (timer_get_conf)(uint8_t timer, uint8_t *st) {
   /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
+  
+  uint8_t readControl = TIMER_RB_CMD | BIT(timer+1) | TIMER_RB_COUNT_;
+  if (sys_outb(TIMER_CTRL, readControl) != OK) {
+      return -1;
+  }
+  int ret;
+  switch (timer)
+  {
+  case 0:
+    ret = util_sys_inb(TIMER_0, st);
+    break;
+  case 1:
+    ret = util_sys_inb(TIMER_1, st);
+    break;
+  case 2:
+    ret = util_sys_inb(TIMER_2, st);
+    break;
+  default:
+    return 1;
+  }
 
-  return 1;
+  return ret;
 }
 
-int (timer_display_conf)(uint8_t timer, uint8_t st,
-                        enum timer_status_field field) {
-  /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
+int (timer_display_conf)(uint8_t timer, uint8_t st, enum timer_status_field field) {
 
-  return 1;
+  union timer_status_field_val conf;
+  switch (field)
+  {
+  case tsf_all:
+    conf.byte = st;
+    break;
+  case tsf_initial:
+    conf.byte = st;
+    conf.in_mode = 14 & st;
+    break;
+  case tsf_mode:
+    conf.byte = st;
+    conf.count_mode = 3;
+    break;
+  case tsf_base:
+    conf.byte = st;
+    conf.bcd = st & 1;
+    break;
+  default:
+    return 1;
+  }
+  timer_print_config(timer, field, conf);
+
+  return 0;
 }
--- lab2/utils.c
+++ lab2/utils.c
@@ -18,7 +18,8 @@ int(util_get_MSB)(uint16_t val, uint8_t *msb) {
 
 int (util_sys_inb)(int port, uint8_t *value) {
   /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
-
-  return 1;
+  uint32_t sol=0;
+  sys_inb(port, &sol);
+  *value=(uint8_t) sol;
+  return 0;
 }
